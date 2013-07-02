d3a182748d6aa87989596177f0279db07907deb0
Ryan Carlson <ryan.t.carlson@cru.org>
2013-07-02 17:49:08 -0400

    Add stateless annotation to Conference Resource

--- src/main/java/org/cru/crs/api/ConferenceResource.java
+++ src/main/java/org/cru/crs/api/ConferenceResource.java
@@ -1,17 +1,22 @@
 package org.cru.crs.api;
 
 import java.util.List;
+import java.util.UUID;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.cru.crs.api.optimizer.ConferenceOptimizer;
 import org.cru.crs.model.ConferenceEntity;
 import org.cru.crs.service.ConferenceService;
 
+@Stateless
 @Path("/conferences")
 public class ConferenceResource
 {
