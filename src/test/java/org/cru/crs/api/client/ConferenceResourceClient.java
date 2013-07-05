6c426afcc6bc9bd7eebf93139032e55d4280c4f1
Ryan Carlson <ryan.t.carlson@cru.org>
2013-07-05 14:15:16 -0400

    Updating test client and test to match change in endpoint from previous commit(s)

--- src/test/java/org/cru/crs/api/client/ConferenceResourceClient.java
+++ src/test/java/org/cru/crs/api/client/ConferenceResourceClient.java
@@ -12,7 +12,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.cru.crs.model.ConferenceEntity;
 import org.jboss.resteasy.client.ClientResponse;
@@ -36,6 +35,7 @@ public interface ConferenceResourceClient
 	public ClientResponse<ConferenceEntity> createConference(ConferenceEntity conference)throws URISyntaxException;
 	
 	@PUT
+	@Path("/{conferenceId}")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public ClientResponse<ConferenceEntity> updateConference(ConferenceEntity conference);
+	public ClientResponse<ConferenceEntity> updateConference(ConferenceEntity conference, @PathParam(value = "conferenceId") String conferenceId);
 }
