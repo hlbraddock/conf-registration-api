a726cb654b89ebb4eed3480bd27f3d45f8bcee32
Ryan Carlson <ryan.t.carlson@cru.org>
2013-07-05 14:02:19 -0400

    Creating page resource client for testing

---
+++ src/test/java/org/cru/crs/api/client/PageResourceClient.java
@@ -0,0 +1,33 @@
+package org.cru.crs.api.client;
+
+import java.net.URISyntaxException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.cru.crs.model.PageEntity;
+
+public interface PageResourceClient
+{
+	@GET
+	@Path("/{pageId")
+	public Response getPage(@PathParam(value="pageId") String pageId);
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createPage(PageEntity newPage, @PathParam(value = "conferenceId") String conferenceId) throws URISyntaxException;	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updatePage(PageEntity page);
+	
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response deletePage(PageEntity page);
+}
