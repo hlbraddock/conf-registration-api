e324bd7e1182fc2e7c64ebf42765fc4c7bcea237
Ryan Carlson <ryan.t.carlson@cru.org>
2013-07-09 15:27:01 -0400

    -Adding ability to create a new block -Updating location header for new page

--- src/main/java/org/cru/crs/api/PageResource.java
+++ src/main/java/org/cru/crs/api/PageResource.java
@@ -1,5 +1,7 @@
 package org.cru.crs.api;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.UUID;
 
 import javax.ejb.Stateless;
@@ -8,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -16,6 +19,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.cru.crs.model.BlockEntity;
 import org.cru.crs.model.ConferenceEntity;
 import org.cru.crs.model.PageEntity;
 import org.cru.crs.service.ConferenceService;
@@ -28,30 +32,30 @@ import com.google.common.base.Preconditions;
 public class PageResource
 {
 	@Inject EntityManager em;
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getPage(@PathParam(value="pageId") UUID pageId)
 	{
 		PageEntity requestedPage = new PageService(em).fetchPageBy(pageId);
-		
+
 		if(requestedPage == null) return Response.status(Status.NOT_FOUND).build();
-		
+
 		return Response.ok(requestedPage).build();
 	}
-	
+
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response updatePage(PageEntity page, @PathParam(value="pageId") UUID pageId)
 	{
 		Preconditions.checkNotNull(pageId);
-		
+
 		PageService pageService = new PageService(em);
-		
+
 		if(pageService.fetchPageBy(pageId) == null)
 		{
 			ConferenceEntity conference = new ConferenceService(em).fetchConferenceBy(page.getConferenceId());
-			
+
 			if(conference != null)
 			{
 				conference.getPages().add(page);
@@ -65,18 +69,34 @@ public class PageResource
 		{
 			pageService.updatePage(page);
 		}
-		
+
 		return Response.noContent().build();
 	}
-	
+
 	@DELETE
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response deletePage(PageEntity page, @PathParam(value="pageId") UUID pageId)
 	{
 		Preconditions.checkNotNull(page.getId());
-		
+
 		new PageService(em).deletePage(page);
-		
+
 		return Response.ok().build();
+	}
+
+	@POST
+	@Path("/blocks/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createBlock(BlockEntity newBlock, @PathParam(value="pageId") UUID pageId) throws URISyntaxException
+	{
+		if(newBlock.getId() == null) newBlock.setId(UUID.randomUUID());
+
+		PageEntity page = new PageService(em).fetchPageBy(pageId);
+
+		if(page == null) return Response.status(Status.BAD_REQUEST).build();
+
+		page.getBlocks().add(newBlock);
+
+		return Response.created(new URI("/blocks/" + newBlock.getId())).build();
 	}
 }
