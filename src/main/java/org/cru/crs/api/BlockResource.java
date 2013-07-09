81216656b0499fca68d5b6ad054fe72f060c8c34
Ryan Carlson <ryan.t.carlson@cru.org>
2013-07-09 14:51:46 -0400

    Adding built out block resource and block service

--- src/main/java/org/cru/crs/api/BlockResource.java
+++ src/main/java/org/cru/crs/api/BlockResource.java
@@ -14,8 +14,11 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.cru.crs.model.BlockEntity;
+import org.cru.crs.model.PageEntity;
+import org.cru.crs.service.BlockService;
 import org.cru.crs.service.PageService;
 
 import com.google.common.base.Preconditions;
@@ -28,27 +31,47 @@ public class BlockResource
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getPage(@PathParam(value="blockId") UUID blockId)
+	public Response getBlock(@PathParam(value="blockId") UUID blockId)
 	{
-		return null;
+		BlockEntity requestedBlock = new BlockService(em).getBlockBy(blockId);
+		
+		if(requestedBlock == null) return Response.status(Status.NOT_FOUND).build();
+		
+		return Response.ok(requestedBlock).build();
 	}
 	
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response updatePage(BlockEntity block, @PathParam(value="blockId") UUID blockId)
+	public Response updateBlock(BlockEntity block, @PathParam(value="blockId") UUID blockId)
 	{
-		Preconditions.checkNotNull(block.getId());
+		Preconditions.checkNotNull(blockId);
 		
+		BlockService blockService = new BlockService(em);
+		
+		if(blockService.getBlockBy(blockId) == null)
+		{
+			PageEntity page = new PageService(em).fetchPageBy(block.getPageId());
+			
+			if(page != null)
+			{
+				page.getBlocks().add(block);
+			}
+			else
+			{
+				return Response.status(Status.BAD_REQUEST).build();
+			}
+		}
 				
 		return Response.noContent().build();
 	}
 	
 	@DELETE
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response deletePage(BlockEntity block, @PathParam(value="blockId") UUID blockId)
+	public Response deleteBlock(BlockEntity block, @PathParam(value="blockId") UUID blockId)
 	{
 		Preconditions.checkNotNull(block.getId());
 		
+		new BlockService(em).deleteBlock(block);
 		
 		return Response.ok().build();
 	}
