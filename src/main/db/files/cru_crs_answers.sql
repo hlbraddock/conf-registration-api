ce8a86a7226db6874633c5f5946cda4de4abb57a
Lee Braddock <lee.braddock@cru.org>
2013-07-12 13:59:25 -0400

    Change table field names.

--- src/main/db/files/cru_crs_answers.sql
+++ src/main/db/files/cru_crs_answers.sql
@@ -2,7 +2,9 @@ DROP TABLE cru_crs_answers;
 
 CREATE TABLE cru_crs_answers (
   id uuid NOT NULL PRIMARY KEY,
-  block uuid,
-  registration uuid,
-  value json
+  registration_id uuid,
+  block_id uuid,
+  answer json
 );
+
+INSERT INTO cru_crs_answers (id,registration_id,block_id,answer) VALUES ('441AD805-7AA6-4B20-8315-8F1390DC4A9E','A2BFF4A8-C7DC-4C0A-BB9E-67E6DCB982E7','AF60D878-4741-4F21-9D25-231DB86E43EE','Incredibly excited about this retreat.');
