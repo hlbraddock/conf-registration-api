e687c75b640f85de0e7ab6449056376cbe64ec84
Ryan Carlson <ryan.t.carlson@cru.org>
2013-07-23 11:13:24 -0400

    JsonDateTimeSerializerTest is now working successfully.

--- src/test/java/org/cru/crs/jaxrs/JsonDateTimeSerializerTest.java
+++ src/test/java/org/cru/crs/jaxrs/JsonDateTimeSerializerTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.cru.crs.api.model.Conference;
 import org.cru.crs.utils.DateTimeCreaterHelper;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -15,8 +16,11 @@ public class JsonDateTimeSerializerTest
 	@Test(groups="unittest")
 	public void testSerializeDateTime() throws JsonGenerationException, JsonMappingException, IOException
 	{
+		Conference conf = new Conference();
+		conf.setEventStartTime(DateTimeCreaterHelper.createDateTime(2013, 4, 10, 0, 30, 45));
+		
 		ObjectMapper mapper = new ObjectMapper();
 		
-		Assert.assertEquals(mapper.writeValueAsBytes(DateTimeCreaterHelper.createDateTime(2013, 4, 10, 0, 30, 45)), "2013-04-10T00:30:45");
+		Assert.assertTrue(mapper.writeValueAsString(conf).contains("\"eventStartTime\":\"2013-04-10T00:30:45.000-04:00\"") );
 	}
 }
