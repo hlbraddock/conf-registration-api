30af1ad81eebe06f264928fbe40af237a9175abe
Ryan Carlson <ryan.t.carlson@cru.org>
2013-07-31 15:41:27 -0400

    Adding servlet to handle populating the user session with Relay SSO GUID

--- src/main/java/org/cru/crs/api/ConferenceResource.java
+++ src/main/java/org/cru/crs/api/ConferenceResource.java
@@ -8,6 +8,8 @@ import java.util.UUID;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -15,6 +17,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -37,6 +40,8 @@ public class ConferenceResource
     @Inject ConferenceService conferenceService;
     @Inject RegistrationService registrationService;
 
+    @Context HttpServletRequest httpRequest;
+    
 	Logger logger = Logger.getLogger(ConferenceResource.class);
 
 	/**
@@ -51,6 +56,7 @@ public class ConferenceResource
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getConferences()
 	{
+		HttpSession session = httpRequest.getSession();
 		return Response.ok(Conference.fromJpa(conferenceService.fetchAllConferences())).build();
 	}
 	
