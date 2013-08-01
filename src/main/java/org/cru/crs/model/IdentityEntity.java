05647f3cdb6ab13cb729f80c0533cc6a35d3f14e
Ryan Carlson <ryan.t.carlson@cru.org>
2013-08-01 13:30:17 -0400

    Adding JPA annotations to these classes

--- src/main/java/org/cru/crs/model/IdentityEntity.java
+++ src/main/java/org/cru/crs/model/IdentityEntity.java
@@ -3,10 +3,14 @@ package org.cru.crs.model;
 import java.util.UUID;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.Type;
 
+@Entity
+@Table(name = "CRU_CRS_IDENTITIES")
 public class IdentityEntity implements java.io.Serializable
 {
 	private static final long serialVersionUID = 1L;
