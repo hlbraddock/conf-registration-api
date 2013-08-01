4a1620f53f2c07a55faed4e8b3bb95c38111fd9c
Ryan Carlson <ryan.t.carlson@cru.org>
2013-08-01 16:41:16 -0400

    Updating database creation scripts to include some basic identity information

--- src/main/db/files/cru_crs_identities.sql
+++ src/main/db/files/cru_crs_identities.sql
@@ -3,3 +3,5 @@ DROP TABLE cru_crs_identities;
 CREATE TABLE cru_crs_identities (
   id uuid NOT NULL PRIMARY KEY
 );
+
+INSERT INTO cru_crs_identities(id) VALUES('f8f8c217-f918-4503-b3b3-85016f9883c1');
