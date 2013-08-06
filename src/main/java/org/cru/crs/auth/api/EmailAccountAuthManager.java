8a7813019523c14ca3d3fe60e27d9ed052dcd2f1
Ryan Carlson <ryan.t.carlson@cru.org>
2013-08-06 13:34:50 -0400

    Adding (now empty) Email auth manager

---
+++ src/main/java/org/cru/crs/auth/api/EmailAccountAuthManager.java
@@ -0,0 +1,38 @@
+package org.cru.crs.auth.api;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.ejb.Stateless;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+import org.cru.crs.utils.AuthCodeGenerator;
+import org.cru.crs.utils.CrsProperties;
+
+@Stateless
+@Path("/auth/email")
+public class EmailAccountAuthManager
+{
+	CrsProperties crsProperties = CrsProperties.get();
+
+	@Path("/login")
+	@GET
+	public Response login(@Context HttpServletRequest httpServletRequest, @QueryParam(value = "code") String code) throws URISyntaxException, MalformedURLException
+	{
+		// generate and store auth code
+		String authCode = AuthCodeGenerator.generate();
+		httpServletRequest.getSession().setAttribute("authCode", authCode);
+
+		// get auth code url from properties
+		String authCodeUrl = crsProperties.getProperty("authCodeUrl");
+
+		// redirect to client managed auth code url with auth code
+		return Response.seeOther(new URI(authCodeUrl + "/" + authCode)).build();
+	}
+}
