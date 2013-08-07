430c4595037e7192d0ef077e93b07cc845e312af
Ryan Carlson <ryan.t.carlson@cru.org>
2013-08-07 15:16:39 -0400

    Updating database build files to include more data for a mock conference/pages/block -Also adding a required field to the blocks table

--- src/main/db/files/cru_crs_pages.sql
+++ src/main/db/files/cru_crs_pages.sql
@@ -8,11 +8,11 @@ CREATE TABLE cru_crs_pages (
 );
 
 INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('7A52AF36-2F3C-5E45-9F76-0AF1-0FF50BB8','42E4C1B2-0CC1-89F7-9F4B-6BC3-E0DB5309',0,'About you');
-INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('0A00D62C-AF29-3723-F949-95A9-50A0B27C','42E4C1B2-0CC1-89F7-9F4B-6BC3-E0DB5309',1,'Ministry preferences');
+INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('0A00D62C-AF29-3723-F949-95A9-50A0B27C','42E4C1B2-0CC1-89F7-9F4B-6BC3-E0DB5309',1,'About your cat');
 INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('7DAE078F-A131-471E-BB70-5156-B62DDEA5','42E4C1B2-0CC1-89F7-9F4B-6BC3-E0DB5309',2,'Hobbies and activities');
-INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('53CCCC6E-0837-A125-93AA-915F-32D1D2E2','1951613E-A253-1AF8-6BC4-C9F1-D0B3FA60',0,'Lorem ipsum dolor sit');
-INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('BF37618E-4F86-2DF5-8AE9-0ED3-BE0ED248','1951613E-A253-1AF8-6BC4-C9F1-D0B3FA60',1,'Lorem ipsum');
-INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('707986E8-F4AD-4F6F-E0FD-D28C-2695B163','D5878EBA-9B3F-7F33-8355-3193-BF4FB698',0,'Lorem ipsum');
+INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('53CCCC6E-0837-A125-93AA-915F-32D1D2E2','1951613E-A253-1AF8-6BC4-C9F1-D0B3FA60',0,'Personal details');
+INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('BF37618E-4F86-2DF5-8AE9-0ED3-BE0ED248','1951613E-A253-1AF8-6BC4-C9F1-D0B3FA60',1,'Cat stories');
+INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('707986E8-F4AD-4F6F-E0FD-D28C-2695B163','D5878EBA-9B3F-7F33-8355-3193-BF4FB698',0,'Epic poetry');
 INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('ECB5B34F-D24D-83C8-2954-2179-AC4C38F2','1CFA829F-2C3A-F803-A966-9A65-10EE2F33',0,'Lorem ipsum dolor');
 INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('A70ECFCE-4967-6EAF-4E15-5A02-FD57D7C7','1CFA829F-2C3A-F803-A966-9A65-10EE2F33',1,'Lorem ipsum dolor sit amet, consectetuer');
 INSERT INTO cru_crs_pages (id,conference_id,position,name) VALUES ('D6BD291C-F82E-3221-387E-89E6-F5372B60','1CFA829F-2C3A-F803-A966-9A65-10EE2F33',2,'Lorem ipsum');
