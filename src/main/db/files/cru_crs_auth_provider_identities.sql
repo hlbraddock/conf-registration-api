7a157808ab46d5f7677690b6c9b004dec78130af
Ryan Carlson <ryan.t.carlson@cru.org>
2013-08-22 16:13:03 -0400

    Removing not null constraint from `cru_crs_auth_provider_identities.auth_provider_username` as there are in fact times when it should be null.

--- src/main/db/files/cru_crs_auth_provider_identities.sql
+++ src/main/db/files/cru_crs_auth_provider_identities.sql
@@ -5,7 +5,7 @@ CREATE TABLE cru_crs_auth_provider_identities(
 	crs_app_id uuid references cru_crs_users(id),
 	auth_provider_id text NOT NULL,
 	auth_provider_name text NOT NULL,
-	auth_provider_username text NOT NULL
+	auth_provider_username text
 );
 
 INSERT INTO cru_crs_auth_provider_identities(id, crs_app_id, auth_provider_id, auth_provider_name, auth_provider_username) VALUES('f8f8c217-f977-4503-b3b3-85016f9883c1', 'f8f8c217-f918-4503-b3b3-85016f9883c1','848392a9-3e58-ed5e-0397-7212128cdf16','RELAY','ryan.t.carlson@cru.org');
