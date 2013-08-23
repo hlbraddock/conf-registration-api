f0ac3362634ceda9f800cc013f2aed4feea1e92f
Ryan Carlson <ryan.t.carlson@cru.org>
2013-08-23 10:51:12 -0400

    clean up warnings

--- src/main/java/org/cru/crs/utils/CrsProperties.java
+++ src/main/java/org/cru/crs/utils/CrsProperties.java
@@ -5,6 +5,8 @@ import org.ccci.util.properties.PropertiesWithFallback;
 
 public class CrsProperties extends PropertiesWithFallback
 {
+	private static final long serialVersionUID = 1L;
+
 	public CrsProperties(CcciProperties.PropertyEncryptionSetup encryptionData, boolean firstSourceOnly, String propertiesFile, String propertiesFile2)
 	{
 		super(encryptionData, firstSourceOnly, propertiesFile, propertiesFile2);
