3366c615855f710c21687ddbfbdc9172d71d8c31
ryancarlson <ryan.t.carlson@cru.org>
2013-09-10 10:39:40 -0400

    Renaming cru_crs_blocks relation to blocks

--- src/main/db/files/cru_crs_answers.sql
+++ src/main/db/files/cru_crs_answers.sql
@@ -3,7 +3,7 @@ DROP TABLE cru_crs_answers CASCADE;
 CREATE TABLE cru_crs_answers (
   id uuid NOT NULL PRIMARY KEY,
   registration_id uuid references cru_crs_registrations(id),
-  block_id uuid references cru_crs_blocks(id),
+  block_id uuid references blocks(id),
   answer json
 );
 
