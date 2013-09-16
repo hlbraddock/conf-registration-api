b490895bf2671f69444d7f2723fd9a7bbb700e2e
ryancarlson <ryan.t.carlson@cru.org>
2013-09-16 15:55:23 -0400

    Add test data to a conference, specifically location fields  - Also fix a table renaming issue that somehow got missed in master

--- src/main/db/files/registrations.sql
+++ src/main/db/files/registrations.sql
@@ -7,4 +7,4 @@ CREATE TABLE registrations (
   conference_id uuid references conferences(id)
 );
 
-INSERT INTO cru_crs_registrations (id,user_id,conference_id) VALUES ('A2BFF4A8-C7DC-4C0A-BB9E-67E6DCB982E7','dbc6a808-d7bc-4d92-967c-d82d9d312898','42E4C1B2-0CC1-89F7-9F4B-6BC3E0DB5309');
+INSERT INTO registrations (id,user_id,conference_id) VALUES ('A2BFF4A8-C7DC-4C0A-BB9E-67E6DCB982E7','dbc6a808-d7bc-4d92-967c-d82d9d312898','42E4C1B2-0CC1-89F7-9F4B-6BC3E0DB5309');
