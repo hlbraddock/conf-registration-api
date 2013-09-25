15fdec15b253b18a39d6b310d1746b3052966aef
Lee Braddock <lee.braddock@cru.org>
2013-09-25 09:19:51 -0400

    Removed extraneous line of data.

--- src/main/db/files/sessions.sql
+++ src/main/db/files/sessions.sql
@@ -7,6 +7,4 @@ CREATE TABLE sessions(
 	auth_provider_id uuid references auth_provider_identities(id)
 );
 
-IDENTITY, auth code, timestamp, auth provider id (fk)
-
 INSERT INTO sessions(id, auth_code, expiration, user_auth_provider_id) VALUES ('b8a8c217-f977-4503-b3b3-85016f981234', 'c4d5c217-f918-4503-b3b3-85016f984567',''02-Oct-2014 02:43:14'','f8f8c217-f977-4503-b3b3-85016f9883c1');
