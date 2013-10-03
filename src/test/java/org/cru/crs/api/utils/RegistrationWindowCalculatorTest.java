13da2cfed258db55299775a57b4a3cedb8077c62
ryancarlson <ryan.t.carlson@cru.org>
2013-10-03 10:28:07 -0400

    Adding a test for `RegistrationWindowCalculator` utility

---
+++ src/test/java/org/cru/crs/api/utils/RegistrationWindowCalculatorTest.java
@@ -0,0 +1,12 @@
+package org.cru.crs.api.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ryancarlson
+ * Date: 10/3/13
+ * Time: 10:25 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RegistrationWindowCalculatorTest
+{
+}
