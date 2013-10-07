13c17dd506b72825fe0cf1d3e1476741518365a7
ryancarlson <ryan.t.carlson@cru.org>
2013-10-07 12:58:19 -0400

    bracketology

--- src/main/java/org/cru/crs/payment/authnet/HttpProvider.java
+++ src/main/java/org/cru/crs/payment/authnet/HttpProvider.java
@@ -3,8 +3,8 @@ package org.cru.crs.payment.authnet;
 import java.io.IOException;
 import java.util.Map;
 
-public interface HttpProvider {
-
+public interface HttpProvider
+{
 	public String getContentFromPost(String url, Map<String, String> parameters) throws IOException;
 
 }
