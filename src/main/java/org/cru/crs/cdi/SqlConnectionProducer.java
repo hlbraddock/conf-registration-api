fcbf06dce99b700913494dcf2c66f0d4a23d1695
ryancarlson <ryan.t.carlson@cru.org>
2013-11-01 12:35:28 -0400

    Turn on QuirksMode Postgres

--- src/main/java/org/cru/crs/cdi/SqlConnectionProducer.java
+++ src/main/java/org/cru/crs/cdi/SqlConnectionProducer.java
@@ -2,6 +2,7 @@ package org.cru.crs.cdi;
 
 import javax.enterprise.inject.Produces;
 
+import org.sql2o.QuirksMode;
 import org.sql2o.Sql2o;
 
 public class SqlConnectionProducer
@@ -10,6 +11,6 @@ public class SqlConnectionProducer
 	@Produces 
 	public Sql2o getSqlConnection()
 	{
-		return new Sql2o("jdbc:postgresql://localhost/crsdb", "crsuser", "crsuser");
+		return new Sql2o("jdbc:postgresql://localhost/crsdb", "crsuser", "crsuser",QuirksMode.PostgreSQL);
 	}
 }
