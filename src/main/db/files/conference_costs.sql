28d340401d000b71d47ff08b3cfd3c4ff1f75097
ryancarlson <ryan.t.carlson@cru.org>
2013-11-01 17:34:18 -0400

    fixing relationship b/w conferences and conference_costs. they share the same ID now

--- src/main/db/files/conference_costs.sql
+++ src/main/db/files/conference_costs.sql
@@ -12,5 +12,5 @@ CREATE TABLE conference_costs (
   authnet_token varchar(25)
 );
 
-INSERT INTO conference_costs (id,base_cost,minimum_deposit) VALUES ('d3be03f0-198f-11e3-8ffd-0800200c9a66', '50.00', '10.0');
-INSERT INTO conference_costs (id,base_cost,minimum_deposit,early_registration_discount,early_registration_cutoff,early_registration_amount) VALUES ('abcd03f0-198f-11e3-8ffd-0800200c5555', '75.00', '0.0', true, '31-Mar-2013 23:59:59', '15');
+INSERT INTO conference_costs (id,base_cost,minimum_deposit) VALUES ('42E4C1B2-0CC1-89F7-9F4B-6BC3E0DB5309', '50.00', '10.0');
+INSERT INTO conference_costs (id,base_cost,minimum_deposit,early_registration_discount,early_registration_cutoff,early_registration_amount) VALUES ('D5878EBA-9B3F-7F33-8355-3193BF4FB698', '75.00', '0.0', true, '31-Mar-2013 23:59:59', '15');
