a3205aa42e2f46ee2adef175f4f4c51e8cff168f
ryancarlson <ryan.t.carlson@cru.org>
2013-11-05 09:34:46 -0500

    Cleanup and document/javadoc `RegistrationResource`

--- src/main/java/org/cru/crs/api/RegistrationResource.java
+++ src/main/java/org/cru/crs/api/RegistrationResource.java
@@ -24,17 +24,16 @@ import javax.ws.rs.core.Response.Status;
 
 import org.ccci.util.time.Clock;
 import org.cru.crs.api.model.Answer;
-import org.cru.crs.api.model.Conference;
 import org.cru.crs.api.model.Payment;
 import org.cru.crs.api.model.Registration;
 import org.cru.crs.api.model.utils.RegistrationAssembler;
 import org.cru.crs.api.process.DeepRegistrationUpdate;
+import org.cru.crs.api.process.PaymentProcessor;
 import org.cru.crs.auth.CrsUserService;
 import org.cru.crs.auth.UnauthorizedException;
 import org.cru.crs.auth.authz.AuthorizationService;
 import org.cru.crs.auth.authz.OperationType;
 import org.cru.crs.auth.model.CrsApplicationUser;
-import org.cru.crs.model.ConferenceCostsEntity;
 import org.cru.crs.model.ConferenceEntity;
 import org.cru.crs.model.PaymentEntity;
 import org.cru.crs.model.RegistrationEntity;
@@ -60,15 +59,25 @@ public class RegistrationResource
     @Inject AnswerService answerService;
     
     @Inject AuthorizationService authorizationService;
+    @Inject PaymentProcessor paymentProcess;
     
     @Inject Clock clock; 
-    
-    @Inject AuthnetPaymentProcess paymentProcess;
-    
+        
 	@Context HttpServletRequest request;
 
 	private Logger logger = Logger.getLogger(RegistrationResource.class);
 
+	/**
+	 * Returns a registration resource specified by @param registrationId
+	 * 
+	 * Possible Outcomes:
+	 * 	200 Ok - found registration and the user specified by @param authCode has read access
+	 *  404 Not Found - no registration resource specified by @param registrationId
+	 *  401 Unauthorized - user specified by @param authCode is expired, doesn't exist or doesn't have read access to this registration.
+	 *  
+	 * @param authCode
+	 * @return
+	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getRegistration(@PathParam(value = "registrationId") UUID registrationId, @HeaderParam(value = "Authorization") String authCode)
@@ -80,8 +89,6 @@ public class RegistrationResource
 			CrsApplicationUser crsLoggedInUser = userService.getLoggedInUser(authCode);
 
 			Registration registration = RegistrationAssembler.buildRegistration(registrationId, registrationService, paymentService, answerService);
-
-			logger.info("get registration is " + registration);
 
 			if(registration == null) return Response.status(Status.NOT_FOUND).build();
 
@@ -101,6 +108,19 @@ public class RegistrationResource
 		}
 	}
 
+	/**
+	 * Updates registration resource specified by @param registrationId
+	 * 
+	 * Possible Outcomes:
+	 * 	204 No Content - registration specified by @param registrationId was found and updated. user specified by @param authCode has update access.
+	 *  201 Created - registration specified by @param registrationId was not found but user specified by @param authCode has create access.  registration created.
+	 *  400 Bad Request - path and entity registration id do not match or were null, or the conference the updated registation should belong to doesn't exist
+	 *  401 Unauthorized - user specified by @param authCode is expired, doesn't exist or doesn't have create and/or update access to this registration.
+	 *  502 Bad Gateway - registration specified by @param registrationId was updated, but the payment was not successfully processed.
+	 *  
+	 * @param authCode
+	 * @return
+	 */
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -108,8 +128,12 @@ public class RegistrationResource
 	{
 		try
 		{
+			logger.info("update registration entity " + registrationId + " and auth code " + authCode);
+			
 			CrsApplicationUser crsLoggedInUser = userService.getLoggedInUser(authCode);
 
+			/*If the path registration id and the entity's registration id are both not null and different, then this is a bad request.
+			 * Malicious or not, we don't really know what the user wants to do.*/
 			if(IdComparer.idsAreNotNullAndDifferent(registrationId, registration.getId()))
 			{
 				return Response.status(Status.BAD_REQUEST).build();
@@ -120,18 +144,21 @@ public class RegistrationResource
 				return Response.status(Status.BAD_REQUEST).build();
 			}
 			
-			logger.info("update registration");
-			Simply.logObject(registration, RegistrationResource.class);
+			/*Find the conference that this registration is for.  If we can't find it, then this is a bad request*/
+			ConferenceEntity conferenceEntityForUpdatedRegistration = conferenceService.fetchConferenceBy(registration.getConferenceId());
 
-			ConferenceEntity conferenceEntity = conferenceService.fetchConferenceBy(registration.getConferenceId());
-
-			if(conferenceEntity == null)
+			if(conferenceEntityForUpdatedRegistration == null)
 			{
 				return Response.status(Status.BAD_REQUEST).build();
 			}
 			
+			logger.info("update registration");
+			Simply.logObject(registration, RegistrationResource.class);
+			
 			boolean createdNewRegistration = false;
 			
+			/*creates on the update endpoint are supported, so if the registration here doesn't exist it will be created, otherwise
+			 * it will be updated*/
 			if(registrationService.getRegistrationBy(registrationId) == null)
 			{
 				createdNewRegistration = true;
@@ -140,20 +167,21 @@ public class RegistrationResource
 
 				logger.info("update registration creating");
 
-				authorizationService.authorize(registrationEntity, conferenceEntity, OperationType.CREATE, crsLoggedInUser);
+				authorizationService.authorize(registrationEntity, conferenceEntityForUpdatedRegistration, OperationType.CREATE, crsLoggedInUser);
 				registrationService.createNewRegistration(registrationEntity);
 			}
 			else
 			{
-				authorizationService.authorize(registration.toDbRegistrationEntity(), conferenceEntity, OperationType.UPDATE, crsLoggedInUser);
+				authorizationService.authorize(registration.toDbRegistrationEntity(), conferenceEntityForUpdatedRegistration, OperationType.UPDATE, crsLoggedInUser);
 				new DeepRegistrationUpdate(registrationService, answerService, paymentService, conferenceService).performDeepUpdate(registration);
 			}
 			
+			/*if this update tells us the payment is ready to process, then the payment will be processed*/
 			if(registration.getCurrentPayment() != null && registration.getCurrentPayment().isReadyToProcess())
 			{	
 				try
 				{
-					processPayment(registration.getCurrentPayment(), crsLoggedInUser);
+					paymentProcess.process(registration.getCurrentPayment(), crsLoggedInUser);
 				}
 				catch(IOException e)
 				{
@@ -176,11 +204,24 @@ public class RegistrationResource
 		}
 	}
 
+	/**
+	 * Deletes registration resource specified by @param registrationId
+	 * 
+	 * Possible Outcomes:
+	 * 	204 No Content - registration specified by @param registrationId was found and deleted. user specified by @param authCode has delete access.
+	 *  400 Bad Request - registration specified by @param registrationId was not found.
+	 *  401 Unauthorized - user specified by @param authCode is expired, doesn't exist or doesn't have delete access to this registration.
+	 *  
+	 * @param authCode
+	 * @return
+	 */
 	@DELETE
 	public Response deleteRegistration(@PathParam(value = "registrationId") UUID registrationId, @HeaderParam(value = "Authorization") String authCode)
 	{
 		try
 		{
+			logger.info("delete registration entity " + registrationId + " and auth code " + authCode);
+			
 			CrsApplicationUser crsLoggedInUser = userService.getLoggedInUser(authCode);
 
 			RegistrationEntity registrationEntity = registrationService.getRegistrationBy(registrationId);
@@ -189,15 +230,14 @@ public class RegistrationResource
 			{
 				return Response.status(Status.BAD_REQUEST).build();
 			}
-			logger.info("delete registration entity");
+
 			Simply.logObject(Registration.fromDb(registrationEntity), RegistrationResource.class);
 
-			/*FIXME: expect this to break at run time as the registration may have payments associated with it
-			 * and that is enforced at the database level.  We need to decide if registrations should be allowed
-			 * to be deleted, b/c if so, payments will also be deleted.  That could be a really bad idea.  It's
-			 * probably better to set some status on the registration to "delete" it. 
-			 */
-			authorizationService.authorize(registrationEntity, conferenceService.fetchConferenceBy(registrationEntity.getConferenceId()), OperationType.DELETE, crsLoggedInUser);
+			authorizationService.authorize(registrationEntity, 
+											conferenceService.fetchConferenceBy(registrationEntity.getConferenceId()), 
+											OperationType.DELETE, 
+											crsLoggedInUser);
+			
 			registrationService.deleteRegistration(registrationEntity);
 
 			return Response.noContent().build();
@@ -208,6 +248,17 @@ public class RegistrationResource
 		}
 	}
 
+	/**
+	 * Creates a new answer resource and associates it to registration specified by @param registrationId
+	 * 
+	 * Possible Outcomes:
+	 *  201 Created - @param answer was created and associated to registration specified by @param registrationId
+	 *  400 Bad Request - path and entity registration id do not match or were null, or the registration the new answer should belong to doesn't exist
+	 *  401 Unauthorized - user specified by @param authCode is expired, doesn't exist or doesn't have update access to this registration.
+	 *  
+	 * @param authCode
+	 * @return
+	 */
 	@POST
 	@Path("/answers")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -216,27 +267,30 @@ public class RegistrationResource
 	{
 		try
 		{
+			logger.info("create answer entity " + registrationId + " and auth code " + authCode);
+			
+			CrsApplicationUser crsLoggedInUser = userService.getLoggedInUser(authCode);
+			
+			/*if the path registration id and the entity's registration id are both not null and different, then this is a bad request
+			 * malicious or not, we don't really know what they want to do.*/
 			if(IdComparer.idsAreNotNullAndDifferent(registrationId, newAnswer.getRegistrationId()))
 			{
 				return Response.status(Status.BAD_REQUEST).build();
 			}
+
+			/*go find the registration for the new answer.  if it doesn't exist, then this is a bad request*/
+			RegistrationEntity registrationEntityForNewAnswer = registrationService.getRegistrationBy(registrationId);
+
+			if(registrationEntityForNewAnswer == null) return Response.status(Status.BAD_REQUEST).build();
 			
-			CrsApplicationUser crsLoggedInUser = userService.getLoggedInUser(authCode);
-
-			RegistrationEntity registrationEntity = registrationService.getRegistrationBy(registrationId);
-
-			if(registrationEntity == null) return Response.status(Status.BAD_REQUEST).build();
-			
-			logger.info("create answer with registration entity");
-			Simply.logObject(Registration.fromDb(registrationEntity), RegistrationResource.class);
-
+			/*prep the new answer by ensuring it has an ID set, and that it's registration id is set to the registration id specified
+			 * in the path.  we've already asserted above that the path and entity registraiton id are the same.*/
 			if(newAnswer.getId() == null) newAnswer.setId(UUID.randomUUID());
 			if(newAnswer.getRegistrationId() == null) newAnswer.setRegistrationId(registrationId);
 			
-			logger.info("create answer");
 			Simply.logObject(newAnswer, RegistrationResource.class);
 
-			authorizationService.authorize(registrationEntity, conferenceService.fetchConferenceBy(registrationEntity.getConferenceId()), OperationType.UPDATE, crsLoggedInUser);
+			authorizationService.authorize(registrationEntityForNewAnswer, conferenceService.fetchConferenceBy(registrationEntityForNewAnswer.getConferenceId()), OperationType.UPDATE, crsLoggedInUser);
 			answerService.insertAnswer(newAnswer.toDbAnswerEntity());
 
 			return Response.status(Status.CREATED)
@@ -250,13 +304,26 @@ public class RegistrationResource
 		}
 	}
 
+	/**
+	 * Returns a payment resource specified by @param paymentId
+	 * 
+	 * Possible Outcomes:
+	 * 	200 Ok - found payment specified by @param paymentId and the user specified by @param authCode has read access to the registration specified by @param registrationId
+	 *  404 Not Found - no payment resource specified by @param paymentId
+	 *  401 Unauthorized - user specified by @param authCode is expired, doesn't exist or doesn't have read access to the registration specifed by @param registrationId.
+	 *  
+	 * @param authCode
+	 * @return
+	 */
 	@GET
     @Path("/payment/{paymentId}")
 	@Produces(MediaType.APPLICATION_JSON)
-    public Response getPayment(@PathParam(value = "paymentId") UUID paymentId, @HeaderParam(value = "Authorization") String authCode) throws URISyntaxException
+    public Response getPayment(@PathParam(value = "paymentId") UUID paymentId, @PathParam(value = "registrationId") UUID registrationId, @HeaderParam(value = "Authorization") String authCode) throws URISyntaxException
     {
 		try
 		{
+			logger.info("get payment entity " + paymentId + " and auth code " + authCode);
+			
 			CrsApplicationUser crsLoggedInUser = userService.getLoggedInUser(authCode);
 
 			PaymentEntity paymentEntity = paymentService.fetchPaymentBy(paymentId);
@@ -266,11 +333,11 @@ public class RegistrationResource
 				return Response.status(Status.NOT_FOUND).build();
 			}
 
+			RegistrationEntity registrationEntityForRequestedPayment = registrationService.getRegistrationBy(registrationId);
 			
-			RegistrationEntity registrationEntity = registrationService.getRegistrationBy(paymentEntity.getRegistrationId());
-			
-			authorizationService.authorize(registrationEntity,
-											conferenceService.fetchConferenceBy(registrationEntity.getConferenceId()),
+			/*If the user has read access to the registration this payment goes with, then they also have read access to the payment*/
+			authorizationService.authorize(registrationEntityForRequestedPayment,
+											conferenceService.fetchConferenceBy(registrationEntityForRequestedPayment.getConferenceId()),
 											OperationType.READ,
 											crsLoggedInUser);
 											
@@ -281,28 +348,6 @@ public class RegistrationResource
 		catch(UnauthorizedException e)
     	{
     		return Response.status(Status.UNAUTHORIZED).build();
-    	}
-    }
-	
-    private void processPayment(Payment payment, CrsApplicationUser loggedInUser) throws IOException, UnauthorizedException
-    {
-    	/*make sure the payment is not processed twice in case the client didn't record the fact it was processed*/
-    	PaymentEntity copyOfPaymentFromDatabase = paymentService.fetchPaymentBy(payment.getId());
-    	if(copyOfPaymentFromDatabase.getTransactionTimestamp() == null && copyOfPaymentFromDatabase.getAuthnetTransactionId() == null)
-    	{
-    		ConferenceEntity dbConference = conferenceService.fetchConferenceBy(registrationService.getRegistrationBy(payment.getRegistrationId()).getConferenceId());
-    		ConferenceCostsEntity dbConferenceCosts = conferenceCostsService.fetchBy(dbConference.getConferenceCostsId());
-    		
-    		Long transactionId = paymentProcess.processCreditCardTransaction(Conference.fromDb(dbConference, dbConferenceCosts), payment);
-
-    		payment.setAuthnetTransactionId(transactionId);
-    		payment.setTransactionDatetime(clock.currentDateTime());
-
-    		paymentService.updatePayment(payment.toJpaPaymentEntity());
-    	}
-    	else
-    	{
-    		logger.info("Payment already processed....");
     	}
     }
 }
---
+++ src/main/java/org/cru/crs/api/process/PaymentProcessor.java
@@ -0,0 +1,63 @@
+package org.cru.crs.api.process;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import org.ccci.util.time.Clock;
+import org.cru.crs.api.model.Conference;
+import org.cru.crs.api.model.Payment;
+import org.cru.crs.auth.UnauthorizedException;
+import org.cru.crs.auth.model.CrsApplicationUser;
+import org.cru.crs.model.ConferenceCostsEntity;
+import org.cru.crs.model.ConferenceEntity;
+import org.cru.crs.model.PaymentEntity;
+import org.cru.crs.payment.authnet.Authnet;
+import org.cru.crs.payment.authnet.AuthnetPaymentProcess;
+import org.cru.crs.service.ConferenceCostsService;
+import org.cru.crs.service.ConferenceService;
+import org.cru.crs.service.PaymentService;
+import org.cru.crs.service.RegistrationService;
+
+public class PaymentProcessor
+{
+	
+	RegistrationService registrationService;
+	PaymentService paymentService;
+	ConferenceService conferenceService;
+	ConferenceCostsService conferenceCostsService;
+	Clock clock;
+	AuthnetPaymentProcess paymentProcess;
+	
+	@Inject
+	public PaymentProcessor(RegistrationService registrationService,
+			PaymentService paymentService, ConferenceService conferenceService,
+			ConferenceCostsService conferenceCostsService, Clock clock,
+			@Authnet AuthnetPaymentProcess paymentProcess) {
+		this.registrationService = registrationService;
+		this.paymentService = paymentService;
+		this.conferenceService = conferenceService;
+		this.conferenceCostsService = conferenceCostsService;
+		this.clock = clock;
+		this.paymentProcess = paymentProcess;
+	}
+
+
+	public void process(Payment payment, CrsApplicationUser loggedInUser) throws IOException, UnauthorizedException
+    {
+    	/*make sure the payment is not processed twice in case the client didn't record the fact it was processed*/
+    	PaymentEntity copyOfPaymentFromDatabase = paymentService.fetchPaymentBy(payment.getId());
+    	if(copyOfPaymentFromDatabase.getTransactionTimestamp() == null && copyOfPaymentFromDatabase.getAuthnetTransactionId() == null)
+    	{
+    		ConferenceEntity dbConference = conferenceService.fetchConferenceBy(registrationService.getRegistrationBy(payment.getRegistrationId()).getConferenceId());
+    		ConferenceCostsEntity dbConferenceCosts = conferenceCostsService.fetchBy(dbConference.getConferenceCostsId());
+    		
+    		Long transactionId = paymentProcess.processCreditCardTransaction(Conference.fromDb(dbConference, dbConferenceCosts), payment);
+
+    		payment.setAuthnetTransactionId(transactionId);
+    		payment.setTransactionDatetime(clock.currentDateTime());
+
+    		paymentService.updatePayment(payment.toJpaPaymentEntity());
+    	}
+    }
+}
