f1c4d60aacf81cf3221ee253e229b90390ba12e7
ryancarlson <ryan.t.carlson@cru.org>
2013-11-05 12:08:40 -0500

    Make the database URL, username and password come from the app server properties/config file

--- src/main/java/org/cru/crs/cdi/SqlConnectionProducer.java
+++ src/main/java/org/cru/crs/cdi/SqlConnectionProducer.java
@@ -1,16 +1,20 @@
 package org.cru.crs.cdi;
 
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 
+import org.cru.crs.utils.CrsProperties;
 import org.sql2o.QuirksMode;
 import org.sql2o.Sql2o;
 
 public class SqlConnectionProducer
 {
 
+	@Inject CrsProperties properties;
+	
 	@Produces 
 	public Sql2o getSqlConnection()
 	{
-		return new Sql2o("jdbc:postgresql://localhost/crsdb", "crsuser", "crsuser",QuirksMode.PostgreSQL);
+		return new Sql2o(properties.getProperty("databaseUrl"), properties.getProperty("databaseUsername"), properties.getProperty("databasePassword"), QuirksMode.PostgreSQL);
 	}
 }
