61fd245584c10895b3d4613ad03a8039980c9588
ryancarlson <ryan.t.carlson@cru.org>
2013-12-03 13:55:57 -0500

    Exception handler will now print the stacktrace and no longer report the stacktrace as the entity.  - Updated service URL for CAS integration as well

--- src/main/java/org/cru/crs/api/exception/UncaughtExceptionMapper.java
+++ src/main/java/org/cru/crs/api/exception/UncaughtExceptionMapper.java
@@ -1,7 +1,6 @@
 package org.cru.crs.api.exception;
 
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -20,7 +19,9 @@ public class UncaughtExceptionMapper implements ExceptionMapper<ApplicationExcep
 			return ((WebApplicationException)exception.getCause()).getResponse();
 		}
 		
-		return Response.serverError().header("Error" , exception.getMessage()).entity(exception.getStackTrace()).build();
+		exception.printStackTrace();
+		
+		return Response.serverError().header("Error" , exception.getMessage()).build();
 	}
 
 }
