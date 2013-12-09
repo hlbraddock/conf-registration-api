7bd86947427810faf1ddbdc213b7d5b19808280f
Ryan Carlson <ryancarlson@media-hub.(none)>
2013-12-09 12:14:15 -0500

    Refactoring name of test to `ConferenceServiceTest`

---
+++ src/test/java/org/cru/crs/service/ConferenceServiceTest.java
@@ -0,0 +1,18 @@
+package org.cru.crs.service;
+
+import java.util.List;
+
+import org.sql2o.Sql2o;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * These tests are meant to be run on a fresh database.
+ * @author ryancarlson
+ *
+ */
+public class ConferenceServiceTest
+{
+
+
+}
