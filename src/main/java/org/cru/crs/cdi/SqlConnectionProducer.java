4ae9d58dea80f307b827f24b973ec9c812d9a78e
ryancarlson <ryan.t.carlson@cru.org>
2013-12-17 12:01:01 -0500

    javadoc

--- src/main/java/org/cru/crs/cdi/SqlConnectionProducer.java
+++ src/main/java/org/cru/crs/cdi/SqlConnectionProducer.java
@@ -13,16 +13,9 @@ import org.sql2o.Sql2o;
 import com.google.common.base.Throwables;
 
 /**
- * So this class presents an interesting little 'hack' using the CDI session scope to enable database transactions to work.
- * Normally, I found that injecting an instance of Sql2o for every service that needs one makes a new connection each time.
- * Ideally it would create one and reuse it throughout the request, but that's not the case.  Because a new one gets created
- * each time, that makes transactions impossible across multiple services.
+ * This producer will now live the length of the request, and return the same Sql2o Connection object for each request.  This allows
+ * the connection be transaction enabled across multiple calls to the database.
  * 
- * The workaround is to make the producer method a @SessionScoped bean.  That ensures that the same producers is used each time
- * and returns the same instance of Sql2o, thus allowing transactions.  B/c this app does not actually uses sessions, this session
- * scoped bean is effectively a "Request Scoped" object since the next request that comes in will not be able to access it.
- * 
- * Testing should be done to ensure that this doesn't completely jack the system in terms of memory usage and garbage collection.
  * @author ryancarlson
  *
  */
