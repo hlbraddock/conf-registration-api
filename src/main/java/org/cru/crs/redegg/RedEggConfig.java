0fa038fd7a18db736796ff06f0743d24b4dd901b
ryancarlson <ryan.t.carlson@cru.org>
2013-12-30 15:50:31 -0500

    API key and endpoint now come from properties file.

--- src/main/java/org/cru/crs/redegg/RedEggConfig.java
+++ src/main/java/org/cru/crs/redegg/RedEggConfig.java
@@ -18,8 +18,8 @@ public class RedEggConfig
 	public ErrbitConfig createConfig() throws URISyntaxException
 	{
 		ErrbitConfig config = new ErrbitConfig();
-		config.setEndpoint(new URI("https://errors.uscm.org/notifier_api/v2/notices"));
-		config.setKey("f906c1a1ac9d48c8a492580705dbcdd0");
+		config.setEndpoint(new URI(properties.getProperty("errbitEndpoint")));
+		config.setKey(properties.getProperty("errbitApiKey"));
 		config.setEnvironmentName(properties.getProperty("errbitEnvironment"));
 		
 		return config;
