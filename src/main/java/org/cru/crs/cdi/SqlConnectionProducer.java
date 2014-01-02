fa2ea1559d6079214dbd85f365d72f10e5783784
Lee Braddock <lee.braddock@cru.org>
2014-01-02 17:54:29 -0500

    Register converter with sql2o to convert postgres enum to ProfileType.

--- src/main/java/org/cru/crs/cdi/SqlConnectionProducer.java
+++ src/main/java/org/cru/crs/cdi/SqlConnectionProducer.java
@@ -6,11 +6,17 @@ import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
+import org.cru.crs.domain.ProfileType;
 import org.cru.crs.utils.CrsProperties;
+import org.jboss.logging.Logger;
+import org.postgresql.util.PGobject;
 import org.sql2o.QuirksMode;
 import org.sql2o.Sql2o;
 
 import com.google.common.base.Throwables;
+import org.sql2o.converters.Convert;
+import org.sql2o.converters.ConverterException;
+import org.sql2o.converters.EnumConverter;
 
 /**
  * This producer will now live the length of the request, and return the same Sql2o Connection object for each request.  This allows
@@ -33,8 +39,40 @@ public class SqlConnectionProducer
 		if(sqlConnection == null)
 		{
 			sqlConnection = new org.sql2o.Connection(new Sql2o(properties.getProperty("databaseUrl"), properties.getProperty("databaseUsername"), properties.getProperty("databasePassword"), QuirksMode.PostgreSQL));
+
+			// register with sql2o a converter for reading ProfileType
+			Convert.registerConverter(ProfileType.class, new PostgresPGObjectToEnumConverter(ProfileType.class));
 		}
 		return sqlConnection;
+	}
+
+	private static class PostgresPGObjectToEnumConverter extends EnumConverter
+	{
+		private Logger logger = Logger.getLogger(PostgresPGObjectToEnumConverter.class);
+
+		public PostgresPGObjectToEnumConverter(Class enumType)
+		{
+			super(enumType);
+		}
+
+		@Override
+		public Enum convert(Object o) throws ConverterException
+		{
+			if (o instanceof PGobject)
+			{
+				try
+				{
+					return ProfileType.valueOf(((PGobject) o).getValue().toUpperCase());
+				}
+				catch (Exception e)
+				{
+					logger.error("Could not convert from " + o.getClass() + " to ProfileType enum ", e);
+				}
+			}
+
+			// fallback to default impl
+			return super.convert(o);
+		}
 	}
 
 	public org.sql2o.Connection getTestSqlConnection()
