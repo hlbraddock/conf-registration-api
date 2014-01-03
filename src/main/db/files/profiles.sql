1f715624ae0cb5a3917c8b0268807699fdbddd83
Lee Braddock <lee.braddock@cru.org>
2014-01-03 11:48:18 -0500

    Set appropriate profile table column types to varchar.

--- src/main/db/files/profiles.sql
+++ src/main/db/files/profiles.sql
@@ -10,12 +10,12 @@ CREATE TABLE profiles (
   street text,
   city text,
   state text,
-  zip text,
+  zip varchar(5),
   birth_date text,
-  gender text,
+  gender varchar(1),
   campus text,
   graduation text,
   dormitory text
 );
 
--- INSERT INTO profiles(id) VALUES('ABf8c217-f918-4503-b3b3-85016f9883c1', 'dbc6a808-d7bc-4d92-967c-d82d9d312898');
+-- INSERT INTO profiles (id, user_id, email, firstName, lastName, phone, street, city, state, zip, birth_date, gender, campus, graduation, dormitory) VALUES ('abcdc217-f918-4503-b3b3-85016f9883c1', 'abcdca08-d7bc-4d92-967c-d82d9d312898', 'c.s.lewis@cru.org', 'Clive', 'Lewis', '407-826-2000', '100 Lake Hart Dr', 'Orlando', 'FL', '32832', '11/29/1898', 'M', 'Oxford', '5/1/1923', 'Hall');
