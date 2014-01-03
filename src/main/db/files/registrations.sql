a15cc25c85062ed9fb9fe577043502b604e4a041
Lee Braddock <lee.braddock@cru.org>
2014-01-03 16:07:12 -0500

    Setting registrations table time stamp to time stamp with timezone, as consistent with all other table time stamp columns. This commit is not part of this branches main purpose, but is being included for correctness.

--- src/main/db/files/registrations.sql
+++ src/main/db/files/registrations.sql
@@ -6,7 +6,7 @@ CREATE TABLE registrations (
   conference_id uuid references conferences(id),
   total_due decimal default 0,
   completed boolean default false,
-  completed_timestamp timestamp
+  completed_timestamp timestamp with time zone
 );
 
 INSERT INTO registrations (id,user_id,conference_id) VALUES ('A2BFF4A8-C7DC-4C0A-BB9E-67E6DCB982E7','dbc6a808-d7bc-4d92-967c-d82d9d312898','42E4C1B2-0CC1-89F7-9F4B-6BC3E0DB5309');
