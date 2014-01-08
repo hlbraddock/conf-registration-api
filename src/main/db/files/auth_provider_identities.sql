3ab1408c61de5c9c2e01d2e45d04c0add1a75c4e
Lee Braddock <lee.braddock@cru.org>
2014-01-08 17:19:04 -0500

    Change auth provider name 'EMAIL_ACCOUNT' in db record to a valid AuthenticationProviderType.

--- src/main/db/files/auth_provider_identities.sql
+++ src/main/db/files/auth_provider_identities.sql
@@ -12,5 +12,5 @@ CREATE TABLE auth_provider_identities(
 );
 
 INSERT INTO auth_provider_identities(id, crs_id, user_auth_provider_id, auth_provider_name, username) VALUES('f8f8c217-f977-4503-b3b3-85016f9883c1', 'f8f8c217-f918-4503-b3b3-85016f9883c1','848392a9-3e58-ed5e-0397-7212128cdf16','RELAY','ryan.t.carlson@cru.org');
-INSERT INTO auth_provider_identities(id, crs_id, user_auth_provider_id, auth_provider_name) VALUES('f8f8c217-f977-4503-b3b3-85016f988342', 'f8f8c217-f918-4503-b3b3-85016f988343','848392a9-3e58-ed5e-0397-7212128cdf17','EMAIL_ACCOUNT');
+INSERT INTO auth_provider_identities(id, crs_id, user_auth_provider_id, auth_provider_name) VALUES('f8f8c217-f977-4503-b3b3-85016f988342', 'f8f8c217-f918-4503-b3b3-85016f988343','848392a9-3e58-ed5e-0397-7212128cdf17','FACEBOOK');
 INSERT INTO auth_provider_identities(id, crs_id, user_auth_provider_id, auth_provider_name, username) VALUES('36f19114-f833-4a26-b7ba-b67052b68cea', 'dbc6a808-d7bc-4d92-967c-d82d9d312898','05218422-6bbf-47fb-897c-371c91f87076','RELAY','crs.testuser@crue.org');
