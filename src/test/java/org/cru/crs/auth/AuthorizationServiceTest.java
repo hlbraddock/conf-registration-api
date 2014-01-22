bb35afaf7623b4ec4cb632edb8b14aeff4c10ab5
Lee_Braddock <lee.braddock@cru.org>
2014-01-22 13:06:25 -0500

    Correct Conference spelling.

--- src/test/java/org/cru/crs/auth/AuthorizationServiceTest.java
+++ src/test/java/org/cru/crs/auth/AuthorizationServiceTest.java
@@ -38,58 +38,58 @@ public class AuthorizationServiceTest
 	@Test(groups="dbtest")
 	public void testGetConference()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		
 		/*any user should be able to read conference data*/
-		authorizationService.authorizeConference(northernMichiganConferece, OperationType.READ, UserInfo.Users.TestUser);
-		authorizationService.authorizeConference(northernMichiganConferece, OperationType.READ, UserInfo.Users.Ryan);
-		authorizationService.authorizeConference(northernMichiganConferece, OperationType.READ, UserInfo.Users.Email);
+		authorizationService.authorizeConference(northernMichiganConference, OperationType.READ, UserInfo.Users.TestUser);
+		authorizationService.authorizeConference(northernMichiganConference, OperationType.READ, UserInfo.Users.Ryan);
+		authorizationService.authorizeConference(northernMichiganConference, OperationType.READ, UserInfo.Users.Email);
 	}
 	
 	@Test(groups="dbtest")
 	public void testUpdateConferenceAllowed()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		
 		/*these users have CREATOR and UPDATE permissions*/
-		authorizationService.authorizeConference(northernMichiganConferece, OperationType.UPDATE, UserInfo.Users.TestUser);
-		authorizationService.authorizeConference(northernMichiganConferece, OperationType.UPDATE, UserInfo.Users.Ryan);
+		authorizationService.authorizeConference(northernMichiganConference, OperationType.UPDATE, UserInfo.Users.TestUser);
+		authorizationService.authorizeConference(northernMichiganConference, OperationType.UPDATE, UserInfo.Users.Ryan);
 	}
 	
 	@Test(groups="dbtest", expectedExceptions=UnauthorizedException.class)
 	public void testUpdateConferenceUnauthorized()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		
 		/*this user only has VIEW permissions*/
-		authorizationService.authorizeConference(northernMichiganConferece, OperationType.UPDATE, UserInfo.Users.Email);
+		authorizationService.authorizeConference(northernMichiganConference, OperationType.UPDATE, UserInfo.Users.Email);
 	}
 	
 	@Test(groups="dbtest")
 	public void testAdminConferenceAllowed()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		
 		/*this user has CREATOR permissions*/
-		authorizationService.authorizeConference(northernMichiganConferece, OperationType.ADMIN, UserInfo.Users.TestUser);
+		authorizationService.authorizeConference(northernMichiganConference, OperationType.ADMIN, UserInfo.Users.TestUser);
 	}
 	
 	@Test(groups="dbtest", expectedExceptions=UnauthorizedException.class)
 	public void testAdminConferenceUnauthorizedOne()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		
 		/*this user only has UPDATE permissions*/
-		authorizationService.authorizeConference(northernMichiganConferece, OperationType.ADMIN, UserInfo.Users.Ryan);
+		authorizationService.authorizeConference(northernMichiganConference, OperationType.ADMIN, UserInfo.Users.Ryan);
 	}
 	
 	@Test(groups="dbtest", expectedExceptions=UnauthorizedException.class)
 	public void testAdminConferenceUnauthorizedTwo()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		
 		/*this user only has VIEW permissions*/
-		authorizationService.authorizeConference(northernMichiganConferece, OperationType.ADMIN, UserInfo.Users.Email);
+		authorizationService.authorizeConference(northernMichiganConference, OperationType.ADMIN, UserInfo.Users.Email);
 	}
 	
 	@Test(groups="dbtest", expectedExceptions=UnauthorizedException.class)
@@ -104,7 +104,7 @@ public class AuthorizationServiceTest
 	@Test(groups="dbtest")
 	public void testAdminRegistrationAllowed()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		RegistrationEntity testUserRegistration = registrationService.getRegistrationByConferenceIdUserId(ConferenceInfo.Id.NorthernMichigan,
 																											UserInfo.Id.TestUser);
 		
@@ -112,25 +112,25 @@ public class AuthorizationServiceTest
 																											UserInfo.Id.Email);
 		
 		/*this user has CREATOR permissions on the conference, so he can administer any registration*/
-		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConferece, OperationType.ADMIN, UserInfo.Users.TestUser);
-		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConferece, OperationType.ADMIN, UserInfo.Users.TestUser);
+		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConference, OperationType.ADMIN, UserInfo.Users.TestUser);
+		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConference, OperationType.ADMIN, UserInfo.Users.TestUser);
 	}
 		
 	@Test(groups="dbtest", expectedExceptions=UnauthorizedException.class)
 	public void testAdminRegistrationUnauthorized()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		RegistrationEntity testUserRegistration = registrationService.getRegistrationByConferenceIdUserId(ConferenceInfo.Id.NorthernMichigan,
 																											UserInfo.Id.TestUser);
 		
 		/*this user has UPDATE permissions on the conference, so he cannot administer any registration*/
-		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConferece, OperationType.ADMIN, UserInfo.Users.Ryan);
+		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConference, OperationType.ADMIN, UserInfo.Users.Ryan);
 	}
 	
 	@Test(groups="dbtest")
 	public void testUpdateRegistrationAllowed()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		RegistrationEntity testUserRegistration = registrationService.getRegistrationByConferenceIdUserId(ConferenceInfo.Id.NorthernMichigan,
 																											UserInfo.Id.TestUser);
 		
@@ -138,32 +138,32 @@ public class AuthorizationServiceTest
 																											UserInfo.Id.Email);
 		
 		/*this user has CREATOR permissions on the conference, so he can update any registration*/
-		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConferece, OperationType.UPDATE, UserInfo.Users.TestUser);
-		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConferece, OperationType.UPDATE, UserInfo.Users.TestUser);
+		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConference, OperationType.UPDATE, UserInfo.Users.TestUser);
+		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConference, OperationType.UPDATE, UserInfo.Users.TestUser);
 		
 		/*this user has UPDATE permissions on the conference, so he can update any registration*/
-		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConferece, OperationType.UPDATE, UserInfo.Users.Ryan);
-		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConferece, OperationType.UPDATE, UserInfo.Users.Ryan);
+		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConference, OperationType.UPDATE, UserInfo.Users.Ryan);
+		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConference, OperationType.UPDATE, UserInfo.Users.Ryan);
 		
 		/*this user does not haveUPDATE permissions on the conference, but should be allowed to update his own registration*/
-		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConferece, OperationType.UPDATE, UserInfo.Users.Email);
+		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConference, OperationType.UPDATE, UserInfo.Users.Email);
 	}
 	
 	@Test(groups="dbtest", expectedExceptions=UnauthorizedException.class)
 	public void testUpdateRegistrationUnauthorized()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		RegistrationEntity testUserRegistration = registrationService.getRegistrationByConferenceIdUserId(ConferenceInfo.Id.NorthernMichigan,
 																											UserInfo.Id.TestUser);
 		
 		/*this user has only VIEW permissions on the conference, so he cannot update any registration, other than his own*/
-		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConferece, OperationType.UPDATE, UserInfo.Users.Email);
+		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConference, OperationType.UPDATE, UserInfo.Users.Email);
 	}
 	
 	@Test(groups="dbtest")
 	public void testViewRegistrationAllowed()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		RegistrationEntity testUserRegistration = registrationService.getRegistrationByConferenceIdUserId(ConferenceInfo.Id.NorthernMichigan,
 																											UserInfo.Id.TestUser);
 		
@@ -171,26 +171,26 @@ public class AuthorizationServiceTest
 																											UserInfo.Id.Email);
 		
 		/*this user has CREATOR permissions on the conference, so he can view any registration*/
-		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConferece, OperationType.READ, UserInfo.Users.TestUser);
-		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConferece, OperationType.READ, UserInfo.Users.TestUser);
+		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConference, OperationType.READ, UserInfo.Users.TestUser);
+		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConference, OperationType.READ, UserInfo.Users.TestUser);
 		
 		/*this user has UPDATE permissions on the conference, so he can view any registration*/
-		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConferece, OperationType.READ, UserInfo.Users.Ryan);
-		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConferece, OperationType.READ, UserInfo.Users.Ryan);
+		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConference, OperationType.READ, UserInfo.Users.Ryan);
+		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConference, OperationType.READ, UserInfo.Users.Ryan);
 		
 		/*this user has VIEW permissions on the conference, so he can view any registration*/
-		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConferece, OperationType.READ, UserInfo.Users.Email);
-		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConferece, OperationType.READ, UserInfo.Users.Email);
+		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConference, OperationType.READ, UserInfo.Users.Email);
+		authorizationService.authorizeRegistration(emailRegistration, northernMichiganConference, OperationType.READ, UserInfo.Users.Email);
 	}
 	
 	@Test(groups="dbtest", expectedExceptions=UnauthorizedException.class)
 	public void testViewRegistrationUnauthorized()
 	{
-		ConferenceEntity northernMichiganConferece = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
+		ConferenceEntity northernMichiganConference = conferenceService.fetchConferenceBy(ConferenceInfo.Id.NorthernMichigan);
 		RegistrationEntity testUserRegistration = registrationService.getRegistrationByConferenceIdUserId(ConferenceInfo.Id.NorthernMichigan,
 																											UserInfo.Id.TestUser);
 		
 		/*this user has no permissions on the conference, so he cannot view someone else's registration*/
-		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConferece, OperationType.ADMIN, new CrsApplicationUser(UUID.randomUUID(),  null,  null));
+		authorizationService.authorizeRegistration(testUserRegistration, northernMichiganConference, OperationType.ADMIN, new CrsApplicationUser(UUID.randomUUID(),  null,  null));
 	}
 }
