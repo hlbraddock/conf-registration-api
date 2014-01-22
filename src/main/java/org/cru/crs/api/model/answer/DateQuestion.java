1618f607d5c4f3e575659dc44ea28811ffb3997d
Lee_Braddock <lee.braddock@cru.org>
2014-01-22 16:39:50 -0500

    Use JsonStandardDateTime Serializer and Deserializer on DateQuestion by way of @JsonSerialize

--- src/main/java/org/cru/crs/api/model/answer/DateQuestion.java
+++ src/main/java/org/cru/crs/api/model/answer/DateQuestion.java
@@ -1,17 +1,23 @@
 package org.cru.crs.api.model.answer;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.cru.crs.jaxrs.JsonStandardDateTimeDeserializer;
+import org.cru.crs.jaxrs.JsonStandardDateTimeSerializer;
 import org.joda.time.DateTime;
 
 public class DateQuestion
 {
 	private DateTime text = null;
 
+	@JsonSerialize(using=JsonStandardDateTimeSerializer.class)
 	public DateTime getText()
 	{
 		return text;
 	}
 
+	@JsonDeserialize(using=JsonStandardDateTimeDeserializer.class)
 	public void setText(DateTime text)
 	{
 		this.text = text;
