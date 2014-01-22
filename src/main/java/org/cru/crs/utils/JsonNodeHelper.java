752a190c723fb9b2b0148830a2eaf2ffe7323896
ryancarlson <ryan.t.carlson@cru.org>
2014-01-22 13:42:23 -0500

    Moving `JsonNodeHelper` to src/main/java, and making it threadsafe

--- src/main/java/org/cru/crs/utils/JsonNodeHelper.java
+++ src/main/java/org/cru/crs/utils/JsonNodeHelper.java
@@ -2,7 +2,6 @@ package org.cru.crs.utils;
 
 import java.io.IOException;
 
-import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonProcessingException;
@@ -16,17 +15,17 @@ import org.codehaus.jackson.map.ObjectMapper;
  */
 public class JsonNodeHelper
 {
-	static ObjectMapper mapper = new ObjectMapper();
-	static JsonFactory factory = mapper.getJsonFactory(); // since 2.1 use mapper.getFactory() instead
+	 // since 2.1 use mapper.getFactory() instead
 
 	public static JsonNode toJsonNode(String jsonText) throws JsonProcessingException, IOException
 	{
-		return mapper.readTree(factory.createJsonParser(jsonText));
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.readTree(mapper.getJsonFactory().createJsonParser(jsonText));
 	}
 	
 	public static String toJsonString(JsonNode jsonNode) throws JsonGenerationException, JsonMappingException, IOException
 	{
-		String jsonText = mapper.writeValueAsString(jsonNode);
+		String jsonText = new ObjectMapper().writeValueAsString(jsonNode);
 		if(jsonText == null || jsonText.trim().length() == 0) jsonText="{}";
 		return jsonText;
 	}
