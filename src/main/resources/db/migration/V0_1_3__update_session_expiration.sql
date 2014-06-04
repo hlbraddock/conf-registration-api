0aafdd215ec6e30642d23513eac820b4d7e78007
Lee Braddock <lee.braddock@cru.org>
2014-06-04 15:31:13 -0400

    Set prebuilt session expiration dates in the future.

---
+++ src/main/resources/db/migration/V0_1_3__update_session_expiration.sql
@@ -0,0 +1,4 @@
+UPDATE sessions SET expiration = '02-Oct-2020 02:43:14 UTC' where id = 'b8a8c217-f977-4503-b3b3-85016f981234';
+UPDATE sessions SET expiration = '02-Oct-2020 02:43:14 UTC' where id = 'c8a8c217-f977-4503-b3b3-85016f981234';
+UPDATE sessions SET expiration = '02-Oct-2020 02:43:14 UTC' where id = 'd8a8c217-f977-4503-b3b3-85016f981234';
+UPDATE sessions SET expiration = '02-Oct-2020 02:43:14 UTC' where id = '1234c217-f977-4503-b3b3-85016f981234';
