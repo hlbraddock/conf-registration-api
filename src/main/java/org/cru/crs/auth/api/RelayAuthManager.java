c393312d47ebe64d3917c867db239cc017ddfceb
Lee Braddock <lee.braddock@cru.org>
2014-06-19 12:58:19 -0400

    Add logging to Relay login

--- src/main/java/org/cru/crs/auth/api/RelayAuthManager.java
+++ src/main/java/org/cru/crs/auth/api/RelayAuthManager.java
@@ -4,6 +4,7 @@ import edu.yale.its.tp.cas.client.CASReceipt;
 import edu.yale.its.tp.cas.client.filter.CASFilter;
 import org.cru.crs.auth.model.RelayUser;
 import org.cru.crs.model.SessionEntity;
+import org.jboss.logging.Logger;
 
 import javax.ejb.Stateless;
 import javax.servlet.http.HttpServletRequest;
@@ -20,22 +21,29 @@ import java.net.URISyntaxException;
 @Path("/auth/relay")
 public class RelayAuthManager extends AbstractAuthManager
 {
+	Logger logger = Logger.getLogger(getClass());
+
 	@Path("/login")
 	@GET
 	public Response login(@Context HttpServletRequest httpServletRequest) throws URISyntaxException, MalformedURLException
 	{
+		logger.info("relay login");
 		HttpSession session = httpServletRequest.getSession();
-		
+
 		RelayUser relayUser = RelayUser.fromCasReceipt((CASReceipt)session.getAttribute(CASFilter.CAS_FILTER_RECEIPT));
-		
+
 		persistIdentityAndAuthProviderRecordsIfNecessary(relayUser);
 
 		SessionEntity sessionEntity = persistSession(relayUser);
 
+		String responseUri = crsProperties.getProperty("clientUrl")
+				+ crsProperties.getProperty("authUrlPath")
+				+ "/"
+				+ sessionEntity.getAuthCode();
+
+		logger.info("relay login response " + responseUri);
+
 		// redirect to client managed auth code url with auth code
-		return Response.seeOther(new URI(crsProperties.getProperty("clientUrl") 
-											+ crsProperties.getProperty("authUrlPath") 
-											+ "/"
-											+ sessionEntity.getAuthCode())).build();
+		return Response.seeOther(new URI(responseUri)).build();
 	}
 }
