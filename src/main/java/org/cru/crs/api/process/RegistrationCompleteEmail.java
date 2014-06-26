f77e7f1696c88339a28fbb89404b6c801b9ea93a
ryancarlson <ryan.t.carlson@cru.org>
2014-06-26 17:23:10 -0400

    Only include payment link for conferences which accept online payment.

--- src/main/java/org/cru/crs/api/process/RegistrationCompleteEmail.java
+++ src/main/java/org/cru/crs/api/process/RegistrationCompleteEmail.java
@@ -43,7 +43,7 @@ public class RegistrationCompleteEmail
 			builder.append(HtmlHelper.paragraph(HtmlHelper.bold("Total Cost: ") + "$" + registration.getTotalDue()));
 			builder.append(HtmlHelper.paragraph(HtmlHelper.bold("Total Amount Paid: ") + "$" + registration.getTotalPaid()));
 
-			if(registration.getRemainingBalance().compareTo(BigDecimal.ZERO) > 0)
+			if(registration.getRemainingBalance().compareTo(BigDecimal.ZERO) > 0 && conferenceCostsEntity.isAcceptCreditCards())
 			{
 				builder.append(HtmlHelper.paragraph(HtmlHelper.bold("Balance Due: ") + " $" + registration.getRemainingBalance()));
 				builder.append(HtmlHelper.paragraph("You can pay the balance due here " + HtmlHelper.href(registrationUrl, "Online Registration Payment Page")));
